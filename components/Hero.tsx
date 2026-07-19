17256a427846a4e8aea67c17da986fcef611a53e
Yasmine-El-krati <yasmineelkrati36@gmail.com>
2026-07-19 01:16:36 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,7 +46,7 @@ const Hero: React.FC = () => {
               
               <div className="relative w-full h-full rounded-3xl overflow-hidden glass-card shadow-2xl flex items-center justify-center border-2 border-white/20">
                 <img 
-                  src="components/yasmine.jpeg"
+                  src="my-portfolio/Screenshot 2026-07-19 011442.png"
                   alt={PERSONAL_DETAILS.name} 
                   className="w-full h-full object-cover transform hover:scale-110 transition-transform duration-700"
                 />
